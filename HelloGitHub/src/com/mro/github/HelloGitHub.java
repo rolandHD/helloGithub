84e204c3ac673b244d23f5c7d3b0f8dc941b8d01
RolandHD <m_ro@gmx.de>
2016-09-21 17:46:00 +0200

    Initial Commit of HelloGithub Project

---
+++ HelloGitHub/src/com/mro/github/HelloGitHub.java
@@ -0,0 +1,10 @@
+package com.mro.github;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Github!");
+	}
+
+}
